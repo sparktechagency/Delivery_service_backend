31b042cf9c2922e22d63f62eed8fb5adebfd679b
azizulhoq953 <azizulhoq2041@gmail.com>
2025-06-17 16:23:24 +0600

    firebase-issues

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import { credential } from 'firebase-admin';
 
 
-const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-9a82a1423c.json');  
+const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-8b5ba0629b.json');  
 // const serviceAccount = require('../../parceldeliveryapp-5dc60-firebase-adminsdk-fbsvc-4089ebb548.json');  
 if (admin.apps.length === 0) {
   admin.initializeApp({
