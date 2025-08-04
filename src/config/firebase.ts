5152419db19dcbdd99195f171ef0a55f352c487b
azizulhoq953 <azizulhoq2041@gmail.com>
2025-08-04 11:03:48 +0600

    review

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,8 +2,8 @@ import * as admin from 'firebase-admin';
 import { credential } from 'firebase-admin';
 
 
-// const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-40b9e9f607.json');  
-const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-00237f3c12.json');  
+const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-40b9e9f607.json');  
+// const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-00237f3c12.json');  
 if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: credential.cert(serviceAccount),
