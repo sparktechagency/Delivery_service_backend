243e910eae07d3c95d544f982cc72ff5a67ffff1
azizulhoq953 <azizulhoq2041@gmail.com>
2025-10-06 13:27:44 +0600

    firebase-prod-name-changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import { credential } from 'firebase-admin';
 
 
-const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-40b9e9f607.json');  
+const serviceAccount = require('../../deliverly-launch-firebase-adminsdk-fbsvc-d418593ff7.json');  
 // const serviceAccount = require('../../deliverly-4811e-firebase-adminsdk-fbsvc-00237f3c12.json');  
 if (admin.apps.length === 0) {
   admin.initializeApp({
