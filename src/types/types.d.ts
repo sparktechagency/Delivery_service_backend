b14987531c656eed3104c0dc9b1830abcdbd4a9f
azizulhoq953 <azizulhoq2041@gmail.com>
2025-10-22 17:21:30 +0600

    re-org

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,19 +1,3 @@
-
-// import { IUser } from '../types/interfaces';  // Correct import
-// import { Request } from 'express';
-
-
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       user?: IUser; 
-//       query: {
-//         timeFrame?: string;  
-//       };
-//     }
-//   }
-// }
-
 import { IUser } from '../types/interfaces';
 import { ParsedQs } from 'qs';
 
